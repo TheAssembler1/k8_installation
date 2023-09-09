1918ce31897ad93d0a9cb72219b3f3f7146f7fbf
theassembler1 <TheAssembler1>
2023-09-08 21:53:45 -0500

    ignore preflight

--- master_node.sh
+++ master_node.sh
@@ -5,9 +5,9 @@ systemctl restart docker
 systemctl restart kubelet
 
 echo "initializing kubernetes cluster"
-sudo kubeadm init --control-plane-endpoint kube-master:6443 --pod-network-cidr 192.168.150.0/23 --cri-socket=unix:///var/run/cri-dockerd.sock
+sudo kubeadm init --control-plane-endpoint kube-master:6443 --pod-network-cidr 192.168.150.0/23 --cri-socket=unix:///var/run/cri-dockerd.sock --ignore-preflight-errors=1
 echo "installing networking ext on master node"
-kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=1 --cri-socket=unix:///var/run/cri-dockerd.sock
+kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=1 --cri-socket=unix:///var/run/cri-dockerd.sock --ignore-preflight-errors=1
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
