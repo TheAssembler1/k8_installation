a4a08aa9a52c03c4027f18594db3b222029082af
theassembler1 <TheAssembler1>
2023-09-08 21:52:25 -0500

    more

--- master_node.sh
+++ master_node.sh
@@ -7,7 +7,7 @@ systemctl restart kubelet
 echo "initializing kubernetes cluster"
 sudo kubeadm init --control-plane-endpoint kube-master:6443 --pod-network-cidr 192.168.150.0/23 --cri-socket=unix:///var/run/cri-dockerd.sock
 echo "installing networking ext on master node"
-kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=1
+kubeadm init --pod-network-cidr=192.168.0.0/16 --ignore-preflight-errors=1 --cri-socket=unix:///var/run/cri-dockerd.sock
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
