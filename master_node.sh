b947d8af324caa12047f4e77993caf0725385f08
theassembler1 <TheAssembler1>
2023-09-08 21:29:21 -0500

    change master node

--- master_node.sh
+++ master_node.sh
@@ -1,10 +1,7 @@
-echo "installing network on master node"
+echo "initializing kubernetes cluster"
+sudo kubeadm init --control-plane-endpoint kube-master:6443 --pod-network-cidr 192.168.150.0/23
+echo "installing networking ext on master node"
 kubeadm init --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-
-echo "installing calico and applying it to kubectl"
-curl https://docs.projectcalico.org/manifests/calico.yaml -O
-kubectl apply -f calico.yaml
-
