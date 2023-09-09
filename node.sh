b2c43ffa7c9e1ea9d6bd69d62cef72be617920ac
theassembler1 <TheAssembler1>
2023-09-08 20:12:49 -0500

    initial commit

---
+++ node.sh
@@ -0,0 +1,49 @@
+
+echo "please ensure required port 6443 is available"
+nc 127.0.0.1 6443
+
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) echo "yes";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
+
+echo "installing docker engine"
+echo "removing possibly conflicting packages"
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+echo "installing docker engine apt repository"
+apt-get update
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+echo "installing docker packages"
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "verifying docker installed successfully"
+docker run hello-world
+
+echo "installing cri-dockerd"
+wget -O "/tmp-cri-dockerd" 'https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.4/cri-dockerd_0.3.4.3-0.ubuntu-jammy_amd64.deb' &&
+sudo dpkg -i "/tmp-cri-dockerd"
+rm -f "/tmp-cri-dockerd"
+
+echo "installing kubelet kubeadm kubect1"
+mkdir -m 755 /etc/apt/keyrings
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+echo "installation may not work becuase cgroup driver is not setup properly!"
